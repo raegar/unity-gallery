866f9fa9169e9e7dac723beb3a3bcffa4aeb8547
raegar <jamie.myland@aru.ac.uk>
2025-02-11 15:35:30 +0000

    Added ARU styling

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,7 +1,8 @@
 // src/Gallery.js
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import "./Gallery.css"; // Import the CSS file below
+import logo from "./0_ARU-Peterborough-blue-RGB1.png"; // Import the logo from src
+import "./Gallery.css"; // Import the CSS file
 
 function Gallery() {
   const [games, setGames] = useState([]);
@@ -14,18 +15,30 @@ function Gallery() {
   }, []);
 
   return (
-    <div className="gallery">
-      {games.map((game) => (
-        <div key={game.id} className="gallery-item">
-          <div className="gallery-image-wrapper">
-            <img src={game.thumbnail} alt={game.title} />
+    <div className="gallery-page">
+      <header className="gallery-header">
+        <img src={logo} alt="University Logo" className="gallery-logo" />
+        <h1>Student Game Showcase</h1>
+        <p>
+          Welcome to our gallery of student games. Explore innovative projects
+          and cutting‐edge interactive experiences built by our talented
+          students.
+        </p>
+      </header>
+
+      <div className="gallery">
+        {games.map((game) => (
+          <div key={game.id} className="gallery-item">
+            <div className="gallery-image-wrapper">
+              <img src={game.thumbnail} alt={game.title} />
+            </div>
+            <h3>{game.title}</h3>
+            <Link className="play-button" to={`/play/${game.id}`}>
+              Play Game
+            </Link>
           </div>
-          <h3>{game.title}</h3>
-          <Link className="play-button" to={`/play/${game.id}`}>
-            Play Game
-          </Link>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
